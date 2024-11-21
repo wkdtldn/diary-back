1129eed9a31431117fcc109c11656f5e1629b3dc
wkdtldn <matteo.jsw@gmail.com>
2024-11-21 20:47:56 +0900

    add s3 set

--- app/diary/routing.py
+++
@@ -1,7 +0,0 @@
-# routing.py
-from django.urls import path
-from . import consumers  # WebSocket 소비자 모듈
-
-websocket_urlpatterns = [
-    path("ws/activity/", consumers.ActivityConsumer.as_asgi()),  # WebSocket 경로
-]
--- back/settings.py
+++ back/settings.py
@@ -50,8 +50,12 @@ ALLOWED_HOSTS = ["*"]
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+from decouple import config
+
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+
+print(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
 AWS_STORAGE_BUCKET_NAME = "dailydiaryappbucket"
 AWS_S3_REGION_NAME = "ap-northeast-2"
 AWS_S3_FILE_OVERWRITE = False
@@ -72,6 +76,7 @@ INSTALLED_APPS = [
     "django_extensions",
     "corsheaders",
     "diary",
+    "oauth2_provider",
     "storages",
 ]
 
